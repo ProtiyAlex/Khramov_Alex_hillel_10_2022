74e2b86191e815082316446f849fc4a9b53450cb
ProtiyAlex <60171936+ProtiyAlex@users.noreply.github.com>
2022-11-14 20:04:40 +0200

    test commit

--- lesson_5m/Colorizer.py
+++ lesson_5m/Colorizer.py
@@ -1,25 +1,27 @@
 class Colorizer:
-    colors = {"pink": '\033[95m',
-              "blue": '\033[94m',
-              "green": '\033[92m',
-              "yellow": '\033[93m',
-              "red": '\033[91m',
-              "bold": '\033[1m',
-              "underline": '\033[4m', }
+    colors = {
+        "pink": "\033[95m",
+        "blue": "\033[94m",
+        "green": "\033[92m",
+        "yellow": "\033[93m",
+        "red": "\033[91m",
+        "bold": "\033[1m",
+        "underline": "\033[4m",
+    }
 
     def __init__(self, color: str):
         self.color = color.lower()
 
     def __enter__(self):
         try:
-            print(self.colors[self.color], end='')
+            print(self.colors[self.color], end="")
         except Exception:
             print(f"\033[91m{self.color} no such color Colorizer \033[0m")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print('\033[0m', end='')
+        print("\033[0m", end="")
 
 
-with Colorizer('red'):
-    print('printed in red')
-print('printed in default color')
+with Colorizer("red"):
+    print("printed in red")
+print("printed in default color")
--- lesson_5m/frange.py
+++ lesson_5m/frange.py
@@ -13,14 +13,14 @@ def frange(start, stop=None, step=None):
         start = start + step
 
 
-assert (list(frange(5)) == [0, 1, 2, 3, 4])
-assert (list(frange(2, 5)) == [2, 3, 4])
-assert (list(frange(2, 10, 2)) == [2, 4, 6, 8])
-assert (list(frange(10, 2, -2)) == [10, 8, 6, 4])
-assert (list(frange(2, 5.5, 1.5)) == [2, 3.5, 5])
-assert (list(frange(1, 5)) == [1, 2, 3, 4])
-assert (list(frange(0, 5)) == [0, 1, 2, 3, 4])
-assert (list(frange(0, 0)) == [])
-assert (list(frange(100, 0)) == [])
+assert list(frange(5)) == [0, 1, 2, 3, 4]
+assert list(frange(2, 5)) == [2, 3, 4]
+assert list(frange(2, 10, 2)) == [2, 4, 6, 8]
+assert list(frange(10, 2, -2)) == [10, 8, 6, 4]
+assert list(frange(2, 5.5, 1.5)) == [2, 3.5, 5]
+assert list(frange(1, 5)) == [1, 2, 3, 4]
+assert list(frange(0, 5)) == [0, 1, 2, 3, 4]
+assert list(frange(0, 0)) == []
+assert list(frange(100, 0)) == []
 
-print('SUCCESS!')
+print("SUCCESS!")
